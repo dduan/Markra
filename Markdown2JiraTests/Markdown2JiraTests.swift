a176331552f9767f0fa920517105e44a2e068015
Daniel Duan <daniel@duan.ca>
2020-10-11 16:16:44 -0700

    Add tests

--- Markdown2JiraTests/Markdown2JiraTests.swift
+++ Markdown2JiraTests/Markdown2JiraTests.swift
@@ -30,4 +30,115 @@ class Markdown2JiraTests: XCTestCase {
             "{{Hello}}"
         )
     }
+
+    func testCodeBlock() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                ```swift
+                func f() {}
+                ```
+                """
+            ),
+            """
+            {code:swift}
+            func f() {}
+            {code}
+            """
+        )
+    }
+
+    func testBlockQuote() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                > hello
+                > world
+                """
+            ),
+            """
+            {quote}
+            hello
+            world
+            {quote}
+            """
+        )
+    }
+
+    func testHeader() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                # one
+                ## two
+                ### three
+                #### four
+                """
+            ),
+            """
+            h1. one
+
+            h2. two
+
+            h3. three
+
+            h4. four
+            """
+        )
+    }
+
+    func testOrderedList() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                1. one
+                2. two
+                3. three
+                """
+            ),
+            """
+            # one
+            # two
+            # three
+            """
+        )
+    }
+
+    func testUnorderedList() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                * one
+                * two
+                * three
+                """
+            ),
+            """
+            * one
+            * two
+            * three
+            """
+        )
+    }
+
+    func testNestedLists() {
+        XCTAssertEqual(
+            markdown2Jira(
+                """
+                * one
+                * two
+                  1. three
+                  2. four
+                * five
+                """
+            ),
+            """
+            * one
+            * two
+            *# three
+            *# four
+            * five
+            """
+        )
+    }
 }
